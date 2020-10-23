c60f4a758048644e374f43f8eaa13dc4fdb53b95
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2020-10-23 10:50:20 -0300

    update

--- ex1.sv
+++ ex1.sv
@@ -1,15 +1,26 @@
 // Questao 1
 always_comb begin
-  if (SWI[6] & SWI[7] & SWI[5]) // b
-    LED[2] <= 1;
-  else if (SWI[4] & SWI[5]) // a
-    LED[2] <= 1;
-  else  // O EXPEDIENTE AINDA NAO ACABOU
+  if ( SWI[5] ) begin 
+    if ( SWI[6] ) begin
+      if ( SWI[7] ) begin
+        LED[2] <= 1;
+      end
+    end else begin
+      if ( SWI[4] ) begin
+        LED[2] <= 1;
+      end else begin
+        LED[2] <= 0;
+      end
+    end
+  end else begin
     LED[2] <= 0;
+  end
 end
 
 //Questao 2
-SEG[0] <= (SWI[0] & !SWI[1]) | (SWI[0] & SWI[2]);
+always_comb begin
+  SEG[0] <= (SWI[0] & !SWI[1]) | (SWI[0] & SWI[2]);
+end
 
 // Questao 3
 always_comb begin
@@ -22,5 +33,7 @@ always_comb begin
 end 
 
 // Questao 4
-LED[0] <= (!SWI[0]); // LAVATORIO FEMININO LIVRE
-LED[1] <= (!SWI[1] | !SWI[2]); // LAVATORIO MASCULINO LIVRE
+always_comb begin
+  LED[0] <= (!SWI[0]); // LAVATORIO FEMININO LIVRE
+  LED[1] <= (!SWI[1] | !SWI[2]); // LAVATORIO MASCULINO LIVRE
+end
