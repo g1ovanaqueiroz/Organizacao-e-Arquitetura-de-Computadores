e6bb2cec53ca1da283e2aca84034ac623c0f4e76
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2020-11-03 14:40:12 -0300

    hamming code

--- Algoritmos de detecção de erro/hamming.py
+++ Algoritmos de detecção de erro/hamming.py
@@ -6,6 +6,12 @@ def int_to_bin(number, padding):
     return (bin(number)[2:]).zfill(padding)
 
 """
+Função que converte uma string de binários para inteiro.
+"""
+def bin_to_int(binary):
+    return int(binary, 2)
+
+"""
 Função que retorna a quantidade de potências de dois existentes até o limite n (len de binary).
 O while usa a expressão (2**i)-1, pois python conta índices de 0 até n-1, e não de 1 até n.
 """
@@ -112,11 +118,23 @@ def encode(binary):
   return list_to_string(result)
 
 """
-Função que realiza a decodificação do código de Hamming.
-Retorna True quando não há erro, e False caso haja erro.
+Função que corrige um binário recebendo o index do bit que está errado.
+"""
+def corrector(binary, index):
+  result = ""
+  for i in range(len(binary)):
+    if i == (index-1):
+      if(binary[i] == '0'): result += '1'
+      else: result += '0'
+    else:
+      result += binary[i]
+  return result
+
+"""
+Função que realiza a decodificação e a correção do código de Hamming.
 """
 def decode(binary):
-  # Lista contendo os binários dos índices dos uns de binary
+   # Lista contendo os binários dos índices dos uns de binary
   list_ones = get_index_element_equals_1(binary)
 
   # Fazendo xor nos índices dos uns
@@ -125,7 +143,51 @@ def decode(binary):
   # Índices dos uns contidos em result
   ones_of_result = get_index_element_equals_1(result)
 
-  return ones_of_result == []
+  if ones_of_result == []:
+    print("\nSem erros! :)")
+  else:
+    data = list(binary)
+    c,ch,error,h,parity_list,h_copy=0,0,0,[],[],[]
+
+    for i in range(len(data)):
+      p=(2**c)
+      h.append(int(data[i]))
+      h_copy.append(data[i])
+      if(p==(i+1)):
+          c=c+1
+
+    for parity in range((len(h))):
+      ph=(2**ch)
+      if(ph==(parity+1)):
+
+          startIndex=ph-1
+          i=startIndex
+          toXor=[]
+
+          while(i<len(h)):
+              block=h[i:i+ph]
+              toXor.extend(block)
+              i+=2*ph
+
+          for z in range(1,len(toXor)):
+              h[startIndex]=h[startIndex]^toXor[z]
+          parity_list.append(h[parity])
+          ch+=1
+    parity_list.reverse()
+    error=sum(int(parity_list) * (2 ** i) for i, parity_list in enumerate(parity_list[::-1]))
+
+    # Caso não encontre erro
+    if((error)==0):
+      print("\nSem erro! :)")
+
+    # Caso erro esteja fora do escopo
+    elif((error)>=len(h_copy)):
+      print('\nErro não pôde ser detectado :/')
+
+    # Caso erro tenha sido enconrtado
+    else:
+      print('\nErro foi encontrado no bit ' + str(error) + " :c\nA mensagem correta deveria ser:")
+      print(corrector(binary, error))
 
 
 """
@@ -133,10 +195,10 @@ def decode(binary):
 * Codificação -> entrada: 0011  saída: 1000011
                  entrada: 0101011   saída: 11001010011
 
-* Decodificação -> entrada: 1000011   saída: True
-                   entrada: 1100011   saída: False
-                   entrada: 11001010011   saída: True
-                   entrada: 10001010011   saída: False
+* Decodificação -> entrada: 1000011   saída: Sem erro! :)
+                   entrada: 1100011   saída: Erro no bit 2
+                   entrada: 11001010011   saída: Sem erro! :)
+                   entrada: 10001010011   saída: Erro no bit 2
                    
 """
 
@@ -153,8 +215,7 @@ while True:
   if entrada == '2':
     print("\nDigite o binário desejado:")
     binary = input()
-    if(decode(binary)): print("\nSem erros! :)")
-    else: print("\niiih... Algo de errado não está certo...")
+    decode(binary)
     break
   else:
     print("Opção inválida, tente novamente!")
