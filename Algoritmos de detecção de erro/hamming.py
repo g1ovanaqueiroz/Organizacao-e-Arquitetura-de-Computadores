2a4f65007f75f2c2cb23992bd1863c5fb61600df
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2020-11-03 00:40:05 -0300

    bit de paridade

--- Algoritmos de detecção de erro/hamming.py
+++ Algoritmos de detecção de erro/hamming.py
@@ -1,4 +1,10 @@
 """
+Função que converte uma string de binários para um inteiro.
+"""
+def bin_to_int(binary):
+    return int(binary, 2)
+
+"""
 Função que retorna um lista que guarda todas as posições de potências de 2 possíveis de acordo com o tamanho da cadeia de bits recebida. n é o limite superior.
 O while usa a expressão (2**i)-1, pois python conta índices de 0 até n-1, e não de 1 até n.
 """
@@ -8,10 +14,15 @@ def get_index_range_power2(n):
   while (2**i)-1 <= n:
     index_list.append((2**i)-1)
     i += 1
-  return index_power2
+  return index_list
 
+
+"""
+Função que retorna uma lista com todos os índices (em binário) dos bits iguais a '1' da cadeia de bits recebida como parâmetro.
+"""
 def get_index_element_equals_1(binary):
   index_list = []
-  for i in range(len(bin)):
-    if bin[i] == '1':
-      index_list.append(i)
+  for i in range(len(binary)):
+    if binary[i] == '1':
+      index_list.append(bin_to_int(i))
+  return index_list
