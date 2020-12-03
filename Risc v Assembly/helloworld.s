428902a995acfdb67164f5a69487b9db8ee4648c
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2020-12-03 15:08:46 -0300

    hello world assembly

---
+++ Risc v Assembly/helloworld.s
@@ -0,0 +1,16 @@
+.data
+msg:.asciiz "Hello world!"
+
+.text
+lb a1,(0)gp
+lb a2,(1)gp
+lb a3,(2)gp
+lb a4,(3)gp
+lb a5,(4)gp
+lb a6,(5)gp
+lb a7,(6)gp
+lb s2,(7)gp
+lb s3,(8)gp
+lb s4,(9)gp
+lb s5,(10)gp
+lb s6,(11)gp
