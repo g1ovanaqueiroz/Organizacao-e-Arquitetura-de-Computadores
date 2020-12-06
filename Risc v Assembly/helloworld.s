558ce9e064cde5fa540fa3a6cf1c2d9e7dde5f58
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2020-12-06 04:27:59 -0300

    hw

--- Risc v Assembly/helloworld.s
+++ Risc v Assembly/helloworld.s
@@ -1,16 +1,6 @@
+addi a0, x0, 4
+la a1, helloworld
+ecall
+ 
 .data
-msg:.asciiz "Hello world!"
-
-.text
-lb a1,(0)gp
-lb a2,(1)gp
-lb a3,(2)gp
-lb a4,(3)gp
-lb a5,(4)gp
-lb a6,(5)gp
-lb a7,(6)gp
-lb s2,(7)gp
-lb s3,(8)gp
-lb s4,(9)gp
-lb s5,(10)gp
-lb s6,(11)gp
+helloworld: .asciiz "Hello world!\n"
